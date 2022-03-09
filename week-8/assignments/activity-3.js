4f01efc16c985ba30ade63433b91f96263ab9ffc
Vidhi <vidhipatel3110@gmail.com>
2022-03-08 21:40:18 -0500

    Activity

---
+++ week-8/assignments/activity-3.js
@@ -0,0 +1,49 @@
+async function timeout(ms, callback) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve();
+    }, ms);
+  });
+}
+
+async function generateRandomNumber() {
+  return Math.floor(Math.random() * 40);
+}
+
+async function generateData(callback) {
+  timeout(1000, function () {
+    const data = Array.from({ length: 20 }, generateRandomNumber);
+    callback(data);
+  });
+}
+
+async function convertToFeet(meters, callback) {
+  const feet = meters * 3.2808;
+  timeout(3500, function () {
+    callback(feet);
+  });
+}
+
+async function processData(data, callback) {
+  data.map(function (value) {
+    callback(value);
+  });
+}
+
+async function logResult(meters, feet) {
+  console.log(`Converted ${meters}m to ${feet}ft`);
+}
+
+async function main() {
+  console.log("Start");
+  generateData(function (data) {
+    processData(data, function (value) {
+      convertToFeet(value, function (result) {
+        logResult(value, result);
+      });
+    });
+  });
+  console.log("Finish");
+}
+
+main();
--- week-8/sync.js
+++ week-8/sync.js
@@ -1,44 +1,48 @@
-function inc(a) {
-  return a + 1;
-}
+const v = ms => new Promise(function (resolve) {
+  setTimeout(resolve, ms)
+});
 
-const sum = function (a, b) {
-  return a + b;
-};
+const inc = (a) => v(3000).then(() => a + 1);
 
-const max = (a, b) => (a > b ? a : b);
+const sum = (a, b) => v(3000).then(() => a + b);
 
-const avg = (a, b) => {
-  const s = sum(a, b);
-  return s / 2;
-};
+const max = (a, b) => v(3000).then(() => a > b ? a : b);
+
+const avg = (a, b) => v(3000).then(() => sum(a, b)).then(s => s / 2);
 
 const obj = {
-  name: "Marcus Aurelius",
-  split(sep = " ") {
-    return this.name.split(sep);
-  },
+name: "Marcus Aurelius",
+split(sep = " ") {
+  return v(3000).then(() => this.name.split(sep));
+},
 };
 
 class Person {
-  constructor(name) {
-    this.name = name;
-  }
+constructor(name) {
+  this.name = name;
+}
 
-  static of(name) {
-    return new Person(name);
-  }
+static of(name) {
+  return new Person(name);
+}
 
-  split(sep = " ") {
-    return this.name.split(sep);
-  }
+split(sep = " ") {
+  return v(3000).then(() => this.name.split(sep));
+}
 }
 
 const person = Person.of("Marcus Aurelius");
 
-console.log("inc(5) =", inc(5));
-console.log("sum(1, 3) =", sum(1, 3));
-console.log("max(8, 6) =", max(8, 6));
-console.log("avg(8, 6) =", avg(8, 6));
-console.log("obj.split() =", obj.split());
-console.log("person.split() =", person.split());
+function doLogs() {
+Promise.all([inc(5), sum(1, 3), max(8, 6), avg(8, 6), obj.split(), person.split()])
+.then(values => {
+  console.log("inc(5) =", values[0]);
+  console.log("sum(1, 3) =", values[1]);
+  console.log("max(8, 6) =", values[2]);
+  console.log("avg(8, 6) =", values[3]);
+  console.log("obj.split() =", values[4]);
+  console.log("person.split() =", values[5]);
+});
+}
+
+doLogs();
